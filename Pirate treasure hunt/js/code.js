8973a1bb5a2e798f55512b341ac698dc08de2f41
ann9908 <75615359+ann9908@users.noreply.github.com>
2021-08-16 15:57:45 +0530

    Add files via upload

---
+++ Pirate treasure hunt/js/code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "hockey";
+const accessCode2 = "train";
+const accessCode3 = "blackboard";
+
+function clues() {
+    
+    fill("black")
+    textSize(15)
+    text("Y E H O K C", 100,50)
+    fill("blue")
+    text("Name the Sport", 100,70)
+
+fill("black")
+    textSize(15)
+    text(" _RAIN", 700,170)
+    fill("blue")
+    text("Hint: we go to far off places in it", 700,185)
+
+    fill("black")
+    textSize(15)
+    text("D R A O B B L A C K", 70,180)
+    fill("blue")
+    text(" what is black when clean & white when dirty?", 70,195)
+
+}
